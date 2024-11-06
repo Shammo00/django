3c56c5cadfb953f977b53e0ea621be3c8c234fdc
Shammo00 <altauhid069@gmail.com>
2024-11-06 16:12:44 +0600

    topic

--- learn/views.py
+++ learn/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from.models import Topic
 
 # Create your views here.
 def home (request):
-    return render(request,'learn\index.html')
+    return render(request,'learn/index.html')
+def topics (request):
+    topic = Topic.objects.order_by('name')
+    context = {
+        "topics": topic,
+    }
+    return render(request,'learn/topic.html',context)
