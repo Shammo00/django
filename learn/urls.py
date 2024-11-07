6462575becaec0ba8a9ecfb5da4f2411d55ae851
Shammo00 <altauhid069@gmail.com>
2024-11-07 22:25:18 +0600

    no'

--- learn/urls.py
+++ learn/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.contrib import admin
+from django.conf.urls import include
 
 from . import views
 urlpatterns = [
     path('',views.home , name='home'),
+    path('admin/',include('ca.urls')), 
     path('topics/',views.topics , name='topics'),
+     
 ]
--- learn/views.py
+++ learn/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from.models import Topic
 
+
 # Create your views here.
 def home (request):
     return render(request,'learn/index.html')
@@ -10,3 +11,4 @@ def topics (request):
         "topics": topic
     }
     return render(request,'learn/topic.html',context)
+
